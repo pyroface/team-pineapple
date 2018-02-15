8298c47b60c9c457b313f20a9035a466ede17f43
annaLarsson <32057027+annaLarsson@users.noreply.github.com>
2018-02-15 20:05:48 +0100

    php

---
+++ LoggedIn/profile.php
@@ -0,0 +1,175 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="../css/offcanvas.css" rel="stylesheet">
+    <link href="../css/hamburgers.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/profile.css">
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Varela+Round');
+      a {
+        font-family: 'Varela Round', sans-serif;
+        font-size: 40px;
+      }
+      .bg-own {
+        background: #8e44ad !important; 
+      }
+  
+      .navbar-brand {
+      display: inline-block;
+      padding-top: 0rem;
+      padding-bottom: 0rem;
+      margin-right: 1rem;
+      font-size: 1.25rem;
+      line-height: inherit;
+      white-space: nowrap;
+      }
+      
+      .navbar-dark .navbar-nav .nav-link:focus, .navbar-dark .navbar-nav .nav-link:hover {
+      color: #ffc107!important;
+      }
+  
+  
+
+    </style>
+
+    <title>off-canvas</title>
+  </head>
+        <body class="bg-light">
+          <nav class="navbar fixed-top navbar-dark bg-dark bg-own ">
+            <a class="navbar-brand" href="#">
+            <img src="../img/logo.png" width="40" height="40" alt="">
+          </a>
+            <button class="hamburger hamburger--slider navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+              <span class="hamburger-box">
+              <span class="hamburger-inner navbar-toggler-icon"></span>
+              </span>
+            </button>
+      
+            <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+              <ul class="navbar-nav mr-auto mt-5 text-center">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="game.html">Game</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Team-admin/Leaderboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../index.php">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          
+        
+          <section class="index__profile black-background">
+                <img src="../img/avatar.jpg" alt="game-avatar" class="img-thumbnail rounded mx-auto d-block h-25  rounded-circle">
+                <h1 class="text-light text-center">Joe Doe</h1>
+      
+      
+                    <!-- Trophy-bar -->
+            <div class="container">
+                <div class="row mt-5 align-middle">
+      
+                  <div class="pl-2">
+                    <a class="mr-2 d-inline" href="#">
+                      <img src="../img/trophy.png" width="40" height="40" class="" alt="">
+                    </a>
+                  </div>
+      
+                  <div class="progress w-75 mt-2" style="height: 20px;">
+                    <div class="progress-bar d-inline   bg-secondary mw-100" role="progressbar  " style="width: 75%;" aria-valuenow="75" aria-valuemin="0"
+                      aria-valuemax="100">75%</div>
+                  </div>
+      
+                </div>
+      
+                <!-- Trophy bar ends -->
+      
+      
+                <!-- Task-bar done -->
+      
+      
+                <div class="row mt-5">
+      
+                <div class="pl-2">
+                  <a class="mr-2 d-inline" href="#">
+                    <img src="../img/done.svg" width="40" height="40" class="" alt="">
+                  </a>
+                </div>
+      
+                <div class="w-75 mt-1 ml-1 rounded" style="background: #06ef69e0;">
+                  <h5 class="p-0 mt-0 mb-0 text-nowrap">GO TO KUNGSHALLEN!</h5>
+                </div>
+      
+              </div>
+      
+              <!-- Task-bar done ends -->
+      
+      
+                 <!-- Task-bar done -->
+      
+      
+                <div class="row mt-5">
+      
+                  <div class="pl-2">
+                    <a class="mr-2 d-inline" href="#">
+                      <img src="../img/done.svg" width="40" height="40" class="" alt="">
+                    </a>
+                  </div>
+      
+                  <div class="w-75 mt-1 ml-1 rounded" style="background: #06ef69e0;">
+                    <h5 class="p-0 mt-0 mb-0 text-nowrap">FIND A TREASURE CHEST!</h5>
+                  </div>
+      
+                </div>
+      
+                <!-- Task-bar done ends -->
+      
+      
+                  <!-- Task-bar undone -->
+      
+      
+                  <div class="row mt-5">
+      
+                    <div class="pl-2">
+                      <a class="mr-2 d-inline" href="#">
+                        <img src="../img/undone.svg" width="40" height="40" class="" alt="">
+                      </a>
+                    </div>
+      
+                    <div class="w-75 mt-1 ml-1 rounded" style="background: #06ef69e0;">
+                      <h5 class="p-0 mt-0 mb-0 text-nowrap" style="color: #637486">FIND THREE COINTS!</h5>
+                    </div>
+      
+                  </div>
+      
+                  <!-- Task-bar undone ends -->
+            </div>
+                  
+      
+      
+      
+              </section>
+      
+            
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="../js/offcanvas.js"></script>
+  </body>
+</html>
