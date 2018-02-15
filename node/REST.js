2d20d1232725f98ec2bbae942e1819015391cb30
annaLarsson <32057027+annaLarsson@users.noreply.github.com>
2018-02-15 19:55:18 +0100

    node

---
+++ node/REST.js
@@ -0,0 +1,40 @@
+var mysql = require("mysql");
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+
+   router.get("/coins",function(req,res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+        var query = "SELECT * FROM ??";
+        var table = ["coins"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "coins" : rows});
+            }
+        });
+    });
+   
+   router.post("/users",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+
+
+
+}
+
+module.exports = REST_ROUTER;
