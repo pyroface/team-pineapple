ad1d10585dfa393d801beb9af2fef1b3db385630
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2018-02-26 11:45:13 +0100

    Cleaned up branch for deply to heroku

--- js/offcanvas.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    'use strict'
-  
-    $('[data-toggle="offcanvas"]').on('click', function () {
-      $('.offcanvas-collapse').toggleClass('open'),
-      $('.hamburger').toggleClass('is-active')
-    })
-  })
-
